7df803f67e0e834c90f83f5247685bcacb4b0ab0
freeworld222 <free.world222@gmail.com>
2026-06-25 21:34:45 +0900

    フォーム機能完成

---
+++ confirm.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>お問い合わせフォーム - 確認画面</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <header>
+    <h2>お問い合わせフォーム - 確認画面</h2>
+  </header>
+
+  <aside>
+    <ul>
+      <li><a href="#">トップページ</a></li>
+      <li><a href="#">人気投稿</a></li>
+      <li><a href="#">エンジニアおすすめ商品</a></li>
+      <li><a href="#">エンジニアおすすめ記事</a></li>
+      <li><a href="#">投稿ページ</a></li>
+    </ul>
+  </aside>
+
+  <form action="send.php" method="post">
+
+    <table>
+      <tr>
+        <th>お名前</th>
+        <td>
+          <?php echo $_POST['name']; ?>
+        </td>
+      </tr>
+
+      <tr>
+        <th>会社名</th>
+        <td>
+          <?php echo $_POST['companyName']; ?>
+        </td>
+      </tr>
+
+      <tr>
+        <th>メールアドレス</th>
+        <td>
+          <?php echo $_POST['email']; ?>
+        </td>
+      </tr>
+
+      <tr>
+        <th>年齢</th>
+        <td>
+          <?php echo $_POST['age']; ?>
+        </td>
+      </tr>
+
+      <tr>
+        <th>お問い合わせ内容</th>
+        <td>
+          <?php echo $_POST['message']; ?>
+        </td>
+
+      </tr>
+    </table>
+
+    <input type="hidden" name="name" value="<?php echo $_POST['name']; ?>">
+    <input type="hidden" name="companyName" value="<?php echo $_POST['companyName']; ?>">
+    <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+    <input type="hidden" name="age" value="<?php echo $_POST['age']; ?>">
+    <input type="hidden" name="message" value="<?php echo $_POST['message']; ?>">
+
+    <input type="submit" value="送信">
+    <input type="button" value="戻る" onclick="history.back()">
+
+  </form>
+
+
+</body>
+
+</html>
---
+++ send.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>お問い合わせフォーム - 送信完了画面</title>
+</head>
+
+<body>
+
+  <h1>お問い合わせフォーム - 送信完了画面</h1>
+
+  <?php
+  echo "お問い合わせが送信されました。ありがとうございます！";
+  ?>
+
+  <a href="contact.php">お問い合わせフォームに戻る</a>
+
+</body>
+
+</html>
